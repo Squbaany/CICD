31a0ae8e57df788b11d27b3b8c5d184fe50c64d7
Squbaany <igorpacholski@gmail.com>
2024-05-15 13:59:38 +0200

    added dockstrings

--- cicd-venv/utils.py
+++ cicd-venv/utils.py
@@ -1,11 +1,20 @@
+"""Basic math functions."""
+
 def add(a: int, b: int) -> int:
+    """Add two numbers together and return the result."""
     return a + b
 
+
 def subtract(a: int, b: int) -> int:
+    """Subtract two numbers and return the result."""
     return a - b
 
+
 def multiply(a: int, b: int) -> int:
+    """Multiply two numbers together and return the result."""
     return a * b
 
+
 def divide(a: int, b: int) -> float:
+    """Divide two numbers and return the result."""
     return a / b
